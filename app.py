f5ed2ea34eda68da50d5a0d2c141567bb617c0cc
2023ac05455 <2023ac05455@wilp.bits-pilani.ac.in>
2025-08-05 11:30:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+"""
+Predit using the trained model
+"""
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
@@ -7,6 +10,7 @@ model = joblib.load('random_forest_model.joblib')
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    """Predict"""
     data = request.json
     input_data = np.array(data['input']).reshape(1, -1)
     prediction = model.predict(input_data)

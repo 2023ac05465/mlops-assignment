3285a4165b5a9f8617915154ed4b63b68c81d78c
Ankit Jain <2023ac05465@wilp.bits-pilani.ac.in>
2025-08-09 20:50:04 +0530

    added decision tree algo

--- app.py
+++ app.py
@@ -3,14 +3,24 @@ import joblib
 import numpy as np
 
 app = Flask(__name__)
-model = joblib.load('random_forest_model.joblib')
+model = joblib.load('model/linear_regression_model.joblib')
+model_decision_tree = joblib.load('model/decision_tree_model.joblib')
 
-@app.route('/predict', methods=['POST'])
-def predict():
+@app.route('/predict/linearregression', methods=['POST'])
+def predict_linear_regression():
     data = request.json
     input_data = np.array(data['input']).reshape(1, -1)
     prediction = model.predict(input_data)
     return jsonify({'prediction': prediction.tolist()})
+
+@app.route('/predict/decisiontree', methods=['POST'])
+def predict_decision_tree():
+    data = request.json
+    input_data = np.array(data['input']).reshape(1, -1)
+    prediction = model.predict(input_data)
+    return jsonify({'prediction': prediction.tolist()})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- train.py
+++ train.py
@@ -1,36 +1,114 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error  
+from sklearn.tree import DecisionTreeRegressor 
+from sklearn.metrics import mean_squared_error
 import joblib
 import mlflow
 
+
+# Load the dataset
 print("Loading dataset...")
 data = pd.read_csv('Housing.csv')
 data.head()
-# pre processing of the data set
+
+# Clean the dataset
 data = data.dropna()
+
+# Encode categorical features
+train_df_object = data.select_dtypes(include='object')
+encoders = {} 
+for col in train_df_object.columns:
+    le = LabelEncoder()
+    data[col] = le.fit_transform(data[col])
+    encoders[col] = le
+
+
+# Features (X) and target (y)
 data = data.drop(columns=['ocean_proximity'])
-data.columns
 x = data.drop(columns=['median_house_value'])
 y = data['median_house_value']
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 
+# Before we start training the model, we will set up MLflow to track our experiments.
+# Expirement : In MFLow an expiremnt act as a container that holds all the runs
+# RUN: A run represents a single execution of a machine learning model, includiing its parameters, metrics and results.
 
-# after the split, we need to perform the experiments
+# By default MFLOW will log the runs in the local file system under mlruns directory
 
-mlflow.set_experiment("Housing_Price_Prediction_Linear_Regression")
-with mlflow.start_run():
-    print("Training model...")
-    model = LinearRegression()
-    model.fit(x_train, y_train.values.ravel())  # Flatten y_train to 1D array
-    #
-    mlflow.log_params({"random_state": 42})
-    y_pred =  model.predict(x_test)
-    mse = mean_squared_error(y_test, y_pred)
-    # track metrics through mlflow
-    mlflow.log_metric("mse", mse)
-    joblib.dump(model, 'linear_regression_model.joblib')
-    #track model through mlflow
-    mlflow.log_artifact('linear_regression_model.joblib')
+def train_with_linear_regression():
+    # Set the experiment name
+    mlflow.set_experiment("Housing_Price_Prediction_Linear_Regression")
+    mlflow.set_tracking_uri("http://localhost:5000")
+
+    # Define model hyperparameters
+    params = {
+        "fit_intercept": True,
+    }
+
+    # After setting the  experiment, we can start tracking various aspects of our model such as parameters, metrics, and model itself.
+    with mlflow.start_run():
+
+        # set expreiment tags
+        mlflow.set_tags({"model": "Linear Regression", "dataset": "Housing Prices"})
+
+        # Log model parameters
+        for param, value in params.items():
+            mlflow.log_param(param, value)
+
+        print("Training model...")
+        model = LinearRegression(fit_intercept=True)
+        model.fit(x_train, y_train.values.ravel())  # Flatten y_train to 1D array
+        y_pred =  model.predict(x_test)
+
+        mse = mean_squared_error(y_test, y_pred)
+       # Log evaluation metrics
+        mlflow.log_metric("mean square error", mse)
+        
+        joblib.dump(model, 'model/linear_regression_model.joblib')
+        #track model through mlflow
+        mlflow.log_artifact('model/linear_regression_model.joblib')
+
+
+def train_with_decision_tree():
+    mlflow.set_experiment("Housing_Price_Prediction_Decision_Tree")
+    mlflow.set_tracking_uri("http://localhost:5000")
+    # Define model hyperparameters
+    params = {
+        "random_state": 44,
+        "max_depth": None,
+        "min_samples_split": 2,
+        "min_samples_leaf": 1
+    }
+
+    # After setting the  experiment, we can start tracking various aspects of our model such as parameters, metrics, and model itself.
+    with mlflow.start_run():
+
+        # set expreiment tags
+        mlflow.set_tags({"model": "Decision Tree", "dataset": "Housing Prices"})
+
+        # Log model parameters
+        for param, value in params.items():
+            mlflow.log_param(param, value)
+
+
+        print("Training model...")
+        model = DecisionTreeRegressor(random_state=44,  max_depth=None, min_samples_split=2, min_samples_leaf=1)
+        model.fit(x_train, y_train.values.ravel())  # Flatten y_train to 1D array
+        y_pred =  model.predict(x_test)
+
+        mse = mean_squared_error(y_test, y_pred)
+        # Log evaluation metrics
+        mlflow.log_metric("mean square error", mse)
+    
+        joblib.dump(model, 'model/decision_tree_model.joblib')
+        #track model through mlflow
+        mlflow.log_artifact('model/decision_tree_model.joblib')
+
+if __name__ == "__main__":
+    train_with_linear_regression()
+    train_with_decision_tree()
+    print("Training completed and models saved.")
+
